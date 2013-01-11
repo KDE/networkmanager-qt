641f44620f713510e54670fe35f4cd00abeab877
Jan Grulich <jgrulich@redhat.com>
2013-01-11 18:27:51 +0100

    Add bluetooth setting

---
+++ settings/bluetooth.cpp
@@ -0,0 +1,137 @@
+/*
+    Copyright 2012  Jan Grulich <jgrulich@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "bluetooth.h"
+#include "bluetooth_p.h"
+
+#include <nm-setting-bluetooth.h>
+
+#include <QDebug>
+
+NetworkManager::Settings::BluetoothSettingPrivate::BluetoothSettingPrivate():
+    name(QString("bluetooth")),
+    bdaddr(QByteArray()),
+    type(BluetoothSetting::Unknown)
+{ }
+
+NetworkManager::Settings::BluetoothSetting::BluetoothSetting():
+    Setting(Setting::Bluetooth),
+    d_ptr(new BluetoothSettingPrivate())
+{ }
+
+NetworkManager::Settings::BluetoothSetting::BluetoothSetting(BluetoothSetting * setting):
+    Setting(setting),
+    d_ptr(new BluetoothSettingPrivate())
+{
+    setBluetoothAddress(setting->bluetoothAddress());
+    setType(setting->type());
+}
+
+NetworkManager::Settings::BluetoothSetting::~BluetoothSetting()
+{
+    delete d_ptr;
+}
+
+QString NetworkManager::Settings::BluetoothSetting::name() const
+{
+    Q_D(const BluetoothSetting);
+
+    return d->name;
+}
+
+void NetworkManager::Settings::BluetoothSetting::setBluetoothAddress(const QByteArray& address)
+{
+    Q_D(BluetoothSetting);
+
+    d->bdaddr = address;
+}
+
+QByteArray NetworkManager::Settings::BluetoothSetting::bluetoothAddress() const
+{
+    Q_D(const BluetoothSetting);
+
+    return d->bdaddr;
+}
+
+void NetworkManager::Settings::BluetoothSetting::setType(NetworkManager::Settings::BluetoothSetting::Type type)
+{
+    Q_D(BluetoothSetting);
+
+    d->type = type;
+}
+
+NetworkManager::Settings::BluetoothSetting::Type NetworkManager::Settings::BluetoothSetting::type() const
+{
+    Q_D(const BluetoothSetting);
+
+    return d->type;
+}
+
+bool NetworkManager::Settings::BluetoothSetting::hasSecrets() const
+{
+    return false;
+}
+
+void NetworkManager::Settings::BluetoothSetting::fromMap(const QVariantMap& setting)
+{
+    if (setting.contains(QLatin1String(NM_SETTING_BLUETOOTH_BDADDR))) {
+        setBluetoothAddress(setting.value(QLatin1String(NM_SETTING_BLUETOOTH_BDADDR)).toByteArray());
+    }
+
+    if (setting.contains(QLatin1String(NM_SETTING_BLUETOOTH_TYPE))) {
+        QString type = setting.value(QLatin1String(NM_SETTING_BLUETOOTH_TYPE)).toString();
+
+        if (type == QLatin1String(NM_SETTING_BLUETOOTH_TYPE_DUN)) {
+            setType(Dun);
+        } else if (type == QLatin1String(NM_SETTING_BLUETOOTH_TYPE_PANU)) {
+            setType(Panu);
+        }
+    }
+}
+
+QVariantMap NetworkManager::Settings::BluetoothSetting::toMap() const
+{
+    QVariantMap setting;
+
+    if (!bluetoothAddress().isEmpty()) {
+        setting.insert(QLatin1String(NM_SETTING_BLUETOOTH_BDADDR), bluetoothAddress());
+    }
+
+    if (type() != Unknown) {
+        switch (type()) {
+            case Dun:
+                setting.insert(QLatin1String(NM_SETTING_BLUETOOTH_TYPE), QLatin1String(NM_SETTING_BLUETOOTH_TYPE_DUN));
+                break;
+            case Panu:
+                setting.insert(QLatin1String(NM_SETTING_BLUETOOTH_TYPE), QLatin1String(NM_SETTING_BLUETOOTH_TYPE_PANU));
+                break;
+        }
+    }
+
+    return setting;
+}
+
+void NetworkManager::Settings::BluetoothSetting::printSetting()
+{
+    NetworkManager::Settings::Setting::printSetting();
+
+    qDebug() << "BLUETOOTH ADDRESS - " << bluetoothAddress();
+    qDebug() << "TYPE - " << type();
+}
---
+++ settings/bluetooth.h
@@ -0,0 +1,69 @@
+/*
+    Copyright 2012  Jan Grulich <jgrulich@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef NM_BLUETOOTHSETTING_H
+#define NM_BLUETOOTHSETTING_H
+
+#include "QtNetworkManager-export.h"
+#include "setting.h"
+
+#include <QString>
+
+namespace NetworkManager
+{
+namespace Settings
+{
+    class BluetoothSettingPrivate;
+
+    class NMQT_EXPORT BluetoothSetting : public Setting
+    {
+    Q_DECLARE_PRIVATE(BluetoothSetting)
+    public:
+        enum Type {Unknown = 0, Dun, Panu};
+
+        BluetoothSetting();
+        BluetoothSetting(BluetoothSetting * setting);
+        ~BluetoothSetting();
+
+        QString name() const;
+
+        void setBluetoothAddress(const QByteArray & address);
+        QByteArray bluetoothAddress() const;
+
+        void setType(Type type);
+        Type type() const;
+
+        bool hasSecrets() const;
+
+        void fromMap(const QVariantMap & setting);
+
+        QVariantMap toMap() const;
+
+        //FOR DEBUG
+        void printSetting();
+
+    private:
+        BluetoothSettingPrivate * d_ptr;
+    };
+
+}
+}
+
+#endif // NM_BLUETOOTHSETTING_H
