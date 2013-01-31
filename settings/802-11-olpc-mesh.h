2689c645cee394225b6ea52e5aa535273263a3f7
Jan Grulich <jgrulich@redhat.com>
2013-01-31 16:12:01 +0100

    Add olpcmesh setting

---
+++ settings/802-11-olpc-mesh.h
@@ -0,0 +1,70 @@
+/*
+    Copyright 2012-2013  Jan Grulich <jgrulich@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef NMQT_OLPCMESHSETTING_H
+#define NMQT_OLPCMESHSETTING_H
+
+#include "../QtNetworkManager-export.h"
+#include "setting.h"
+
+#include <QtCore/QString>
+
+namespace NetworkManager
+{
+namespace Settings
+{
+    class OlpcMeshSettingPrivate;
+
+    class NMQT_EXPORT OlpcMeshSetting : public Setting
+    {
+    Q_DECLARE_PRIVATE(OlpcMeshSetting)
+    public:
+        OlpcMeshSetting();
+        OlpcMeshSetting(OlpcMeshSetting *);
+        ~OlpcMeshSetting();
+
+        QString name() const;
+
+        void setSsid(const QByteArray & ssid);
+        QByteArray ssid() const;
+
+        void setChannel(quint32 channel);
+        quint32 channel() const;
+
+        void setDhcpAnycastAddress(const QByteArray & address);
+        QByteArray dhcpAnycastAddress() const;
+
+        bool hasSecrets() const;
+
+        void fromMap(const QVariantMap & setting);
+
+        QVariantMap toMap() const;
+
+        //FOR DEBUG
+        void printSetting();
+
+    private:
+        OlpcMeshSettingPrivate * d_ptr;
+    };
+
+}
+}
+
+#endif // NMQT_OLPCMESHSETTING_H
