c57b00371fd2e4b655c5c6f757ed8072fae9938b
Jan Grulich <jgrulich@redhat.com>
2013-02-11 12:02:26 +0100

    Add test for wimax setting

---
+++ tests/wimaxsetting.cpp
@@ -0,0 +1,59 @@
+/*
+    Copyright 2012-2013  Jan Grulich <jgrulich@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "wimaxsetting.h"
+
+#include "../settings/wimax.h"
+
+#include <nm-setting-wimax.h>
+
+void WimaxSetting::testSetting_data()
+{
+    QTest::addColumn<QString>("networkName");
+    QTest::addColumn<QByteArray>("macAddress");
+
+    QTest::newRow("setting1")
+            << QString("networkname")           // networkName
+            << QByteArray("01:23:45:67:89:ab"); // macAddress
+
+}
+
+void WimaxSetting::testSetting()
+{
+    QFETCH(QString, networkName);
+    QFETCH(QByteArray, macAddress);
+
+    QVariantMap map;
+
+    map.insert(QLatin1String(NM_SETTING_WIMAX_NETWORK_NAME), networkName);
+    map.insert(QLatin1String(NM_SETTING_WIMAX_MAC_ADDRESS), macAddress);
+
+    NetworkManager::Settings::WimaxSetting setting;
+    setting.fromMap(map);
+
+    QVariantMap map1 = setting.toMap();
+
+    foreach (const QString & key, map.keys()) {
+        QCOMPARE(map.value(key), map1.value(key));
+    }
+}
+
+QTEST_MAIN(WimaxSetting)
+#include "wimaxsetting.moc"
