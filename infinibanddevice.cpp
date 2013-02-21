68e7dd5d6326c9ef6995e4b48eff4168ef6614a6
Jan Grulich <jgrulich@redhat.com>
2013-02-21 10:47:29 +0100

    Add infiniband device

---
+++ infinibanddevice.cpp
@@ -0,0 +1,102 @@
+/*
+    Copyright 2013 Jan Grulich <jgrulich@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "infinibanddevice.h"
+#include "device_p.h"
+#include "manager.h"
+#include "manager_p.h"
+
+#include "nm-device-infinibandinterface.h"
+
+namespace NetworkManager
+{
+class InfinibandDevicePrivate : public DevicePrivate
+{
+public:
+    InfinibandDevicePrivate(const QString & path, QObject * owner);
+    virtual ~InfinibandDevicePrivate();
+
+    OrgFreedesktopNetworkManagerDeviceInfinibandInterface iface;
+    bool carrier;
+    QString hwAddress;
+};
+}
+
+NetworkManager::InfinibandDevicePrivate::InfinibandDevicePrivate(const QString& path, QObject* owner):
+    DevicePrivate(path, owner), iface(NetworkManagerPrivate::DBUS_SERVICE, path,QDBusConnection::systemBus()),
+    carrier(false), hwAddress(QString())
+{
+}
+
+NetworkManager::InfinibandDevice::~InfinibandDevice()
+{
+}
+
+NetworkManager::InfinibandDevice::InfinibandDevice(const QString& path, QObject* parent):
+    Device(*new InfinibandDevicePrivate(path, this), parent)
+{
+    Q_D(InfinibandDevice);
+
+    d->carrier = d->iface.carrier();
+    d->hwAddress = d->iface.hwAddress();
+
+    connect(&d->iface, SIGNAL(PropertiesChanged(QVariantMap)),
+            this, SLOT(onPropertiesChanged(QVariantMap)));
+}
+
+NetworkManager::InfinibandDevicePrivate::~InfinibandDevicePrivate()
+{
+}
+
+NetworkManager::Device::Type NetworkManager::InfinibandDevice::type() const
+{
+    return NetworkManager::Device::ADSL;
+}
+
+bool NetworkManager::InfinibandDevice::carrier() const
+{
+    Q_D(const InfinibandDevice);
+
+    return d->carrier;
+}
+
+QString NetworkManager::InfinibandDevice::hwAddress() const
+{
+    Q_D(const InfinibandDevice);
+
+    return d->hwAddress;
+}
+
+void NetworkManager::InfinibandDevice::onPropertiesChanged(const QVariantMap& properties)
+{
+    Q_D(InfinibandDevice);
+
+    if (properties.contains(QLatin1String("Carrier"))) {
+        d->carrier = properties.value(QLatin1String("Carrier")).toBool();
+        emit carrierChanged(d->carrier);
+    }
+
+    if (properties.contains(QLatin1String("HwAddress"))) {
+        d->hwAddress = properties.value(QLatin1String("HwAddress")).toString();
+        emit hwAddressChanged(d->hwAddress);
+    }
+}
+
+#include "infinibanddevice.moc"
---
+++ infinibanddevice.h
@@ -0,0 +1,56 @@
+/*
+    Copyright 2013 Jan Grulich <jgrulich@redhat.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef NMQT_INFINIBAND_DEVICE_H
+#define NMQT_INFINIBAND_DEVICE_H
+
+#include "device.h"
+#include "QtNetworkManager-export.h"
+
+namespace NetworkManager {
+
+class InfinibandDevicePrivate;
+
+class NMQT_EXPORT InfinibandDevice : public Device
+{
+Q_OBJECT
+Q_DECLARE_PRIVATE(InfinibandDevice)
+Q_PROPERTY(bool carrier READ carrier NOTIFY carrierChanged)
+Q_PROPERTY(QString hwAddress READ hwAddress NOTIFY hwAddressChanged)
+
+public:
+    InfinibandDevice(const QString& path, QObject* parent);
+    virtual ~InfinibandDevice();
+
+    virtual Type type() const;
+    bool carrier() const;
+    QString hwAddress() const;
+
+public Q_SLOTS:
+    void onPropertiesChanged(const QVariantMap & properties);
+
+Q_SIGNALS:
+    void carrierChanged(bool plugged);
+    void hwAddressChanged(const QString addres);
+};
+
+}
+
+#endif
