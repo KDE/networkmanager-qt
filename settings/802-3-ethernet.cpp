ae4e1c4c54e9c99d6c7ee324d156fea297532bb6
Lukáš Tinkl <lukas@kde.org>
2013-02-27 01:39:43 +0100

    use the generic types only in impl
    the layout of the global includes with the settings subdir and out-of-band
    generic types is seriously broken...

--- settings/802-3-ethernet.cpp
+++ settings/802-3-ethernet.cpp
@@ -20,6 +20,7 @@
 
 #include "802-3-ethernet.h"
 #include "802-3-ethernet_p.h"
+#include "generic-types.h"
 
 #include <nm-setting-wired.h>
 
@@ -215,14 +216,14 @@ NetworkManager::Settings::WiredSetting::S390Nettype NetworkManager::Settings::Wi
     return d->s390NetType;
 }
 
-void NetworkManager::Settings::WiredSetting::setS390Options(const QStringMap & options)
+void NetworkManager::Settings::WiredSetting::setS390Options(const QMap<QString, QString> &options)
 {
     Q_D(WiredSetting);
 
     d->s390Options = options;
 }
 
-QStringMap NetworkManager::Settings::WiredSetting::s390Options() const
+QMap<QString,QString> NetworkManager::Settings::WiredSetting::s390Options() const
 {
     Q_D(const WiredSetting);
 
--- settings/802-3-ethernet.h
+++ settings/802-3-ethernet.h
@@ -24,8 +24,6 @@
 #include "../QtNetworkManager-export.h"
 #include "setting.h"
 
-#include "generic-types.h"
-
 #include <QtCore/QStringList>
 
 namespace NetworkManager
@@ -78,8 +76,8 @@ public:
     void setS390NetType(S390Nettype type);
     S390Nettype s390NetType() const;
 
-    void setS390Options(const QStringMap &options);
-    QStringMap s390Options() const;
+    void setS390Options(const QMap<QString,QString> &options);
+    QMap<QString,QString> s390Options() const;
 
     bool hasSecrets() const;
 
