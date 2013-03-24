b2630b2545abb97694cd09b0be3c616ee0ace374
Jan Grulich <jgrulich@redhat.com>
2013-03-24 20:55:06 +0100

    Add more comments to the example with creating wireless connection

--- examples/createconnection/main.cpp
+++ examples/createconnection/main.cpp
@@ -46,8 +46,9 @@ int main()
     Settings::ConnectionSettings * settings = new Settings::ConnectionSettings(Settings::ConnectionSettings::Wireless);
     DeviceList deviceList = NetworkManager::networkInterfaces();
 
-    WirelessDevice * wifiDevice;
+    WirelessDevice * wifiDevice = 0;
 
+    // We have to find some wireless device
     foreach (Device * dev, deviceList) {
         if (dev->type() == Device::Wifi) {
             wifiDevice = dynamic_cast<WirelessDevice*>(dev);
@@ -55,11 +56,16 @@ int main()
         }
     }
 
+    if (!wifiDevice) {
+        return 1;
+    }
+
     QStringList accessPointList = wifiDevice->accessPoints();
     QString ssid;
     QString result;
     QString accessPointPath;
 
+    // Check for available accesspoint
     foreach (const QString & ap, accessPointList) {
         AccessPoint accessPoint(ap);
         // For simplification we use APs only with Wep security or without any security
@@ -73,7 +79,7 @@ int main()
             qout << "Yes/No: " << flush;
             qin >> result;
 
-            if (result == "Yes" || result == "yes" || result == "y" || result == "YES") {
+            if (result.toLower() == "yes" || result == "y") {
                 ssid = accessPoint.ssid();
                 accessPointPath = ap;
                 break;
@@ -81,26 +87,32 @@ int main()
         }
     }
 
+    // Now we will prepare our new connection, we have to specify ID and create new UUID
     settings->setId(ssid);
     settings->setUuid(QUuid::createUuid().toString().mid(1, QUuid::createUuid().toString().length() - 2));
 
+    // For wireless setting we have to specify SSID
     Settings::WirelessSetting * wirelessSetting = dynamic_cast<Settings::WirelessSetting *>(settings->setting(Settings::Setting::Wireless));
     wirelessSetting->setSsid(ssid.toAscii());
 
     Settings::Ipv4Setting * ipv4Setting = dynamic_cast<Settings::Ipv4Setting *>(settings->setting(Settings::Setting::Ipv4));
     ipv4Setting->setMethod(Settings::Ipv4Setting::Automatic);
 
+    // We try to add and activate our new wireless connection
     QDBusPendingReply <QDBusObjectPath, QDBusObjectPath > reply = NetworkManager::addAndActivateConnection(settings->toMap(), wifiDevice->uni(), accessPointPath);
 
     reply.waitForFinished();
 
+    // Check if this connection were added successfuly
     if (reply.isValid()) {
+        // Now our connection should be added in NetworkManager and we can print all settings pre-filled from NetworkManager
         Settings::Connection connection(reply.value().path());
         Settings::ConnectionSettings * newSettings = new Settings::ConnectionSettings(Settings::ConnectionSettings::Wireless);
         newSettings->fromMap(connection.settings());
         // Print resulting settings
         newSettings->printSetting();
 
+        // Continue with adding secrets
         Settings::WirelessSecuritySetting * wirelessSecuritySetting = dynamic_cast<Settings::WirelessSecuritySetting *>(newSettings->setting(Settings::Setting::WirelessSecurity));
         if (!wirelessSecuritySetting->needSecrets().isEmpty()) {
             qDebug() << "Need secrets: " << wirelessSecuritySetting->needSecrets();
