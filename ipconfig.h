501480a93169a95432d748d242a9252278bb5470
Daniel Nicoletti <dantti12@gmail.com>
2013-04-03 17:53:34 -0300

    Add documentation to IpConfig classes

--- ipconfig.h
+++ ipconfig.h
@@ -33,13 +33,41 @@ namespace NetworkManager
 class NMQT_EXPORT IpAddress : public QNetworkAddressEntry
 {
 public:
+    /**
+     * Constructs an empty IpAddress object.
+     */
     IpAddress();
-    ~IpAddress();
+
+    /**
+     * Constructs a IpAddress object that is a copy of the object other.
+     */
     IpAddress(const IpAddress&);
+
+    /**
+     * Destroys this IpRoute object.
+     */
+    ~IpAddress();
+
+    /**
+     * Return if the IP address is defined.
+     */
     bool isValid() const;
+
+    /**
+     * Defines the default gateway of this object.
+     */
     void setGateway(const QHostAddress &gateway);
+
+    /**
+     * Returns the default gateway of this object.
+     */
     QHostAddress gateway() const;
+
+    /**
+     * Makes a copy of the IpAddress object other.
+     */
     IpAddress &operator=(const IpAddress&);
+
 private:
     class Private;
     Private * d;
@@ -49,15 +77,52 @@ typedef QList<IpAddress> IpAddresses;
 class NMQT_EXPORT IpRoute : public QNetworkAddressEntry
 {
 public:
+    /**
+     * Constructs an empty IpRoute object.
+     */
     IpRoute();
-    ~IpRoute();
+
+    /**
+     * Constructs a IpRoute object that is a copy of the object other.
+     */
     IpRoute(const IpRoute&);
-    IpRoute &operator=(const IpRoute&);
+
+    /**
+     * Destroys this IpRoute object.
+     */
+    ~IpRoute();
+
+    /**
+     * Returns true if the route IP is defined.
+     */
     bool isValid() const;
+
+    /**
+     * Defines the next hop of the given route.
+     */
     void setNextHop(const QHostAddress &nextHop) const;
+
+    /**
+     * Returns the next hop of the given route.
+     */
     QHostAddress nextHop() const;
+
+    /**
+     * Defines the metric of the given route,
+     * lower values have higher priority.
+     */
     void setMetric(quint32 metric);
+
+    /**
+     * Returns the route metric number of the given route.
+     */
     quint32 metric() const;
+
+    /**
+     * Makes a copy of the IpRoute object other.
+     */
+    IpRoute &operator=(const IpRoute&);
+
 private:
     class Private;
     Private * d;
@@ -67,22 +132,59 @@ typedef QList<IpRoute> IpRoutes;
 class NMQT_EXPORT IpConfig
 {
 public:
+    /**
+     * Constructs a ip config object with a list of address, nameservers, domains and routers.
+     */
     IpConfig(const IpAddresses &addresses,
         const QList<QHostAddress> &nameservers,
         const QStringList &domains,
         const IpRoutes &routes);
-    IpConfig();
-    ~IpConfig();
-    IpConfig(const IpConfig&);
+
     /**
-     * List of IP addresses related to this configuration.
+     * Constructs an empty IpConfig object.
      */
-    IpAddresses addresses() const;
+    IpConfig();
+
+    /**
+     * Destroys this IpConfig object.
+     */
+    ~IpConfig();
+
+    /**
+     * Constructs a IpConfig object that is a copy of the object other.
+     */
+    IpConfig(const IpConfig&);
+
+    /**
+     * Returns a list of IP addresses related to this configuration.
+     */
+    NetworkManager::IpAddresses addresses() const;
+
+    /**
+     * Returns a list of nameservers related to this configuration.
+     */
     QList<QHostAddress> nameservers() const;
+
+    /**
+     * Returns a list of domains related to this configuration.
+     */
     QStringList domains() const;
+
+    /**
+     * Returns a list of routes related to this configuration.
+     */
     IpRoutes routes() const;
+
+    /**
+     * Makes a copy of the IpConfig object other.
+     */
     IpConfig &operator=(const IpConfig&);
+
+    /**
+     * Returns false if the list of IP Addresses is empty
+     */
     bool isValid() const;
+
 private:
     class Private;
     Private * d;
