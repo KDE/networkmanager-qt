73c3790a73cefd5b5ee8f016bc03f842d8d42e03
Lukáš Tinkl <lukas@kde.org>
2013-04-29 13:42:11 +0200

    fix build with Bond setting

--- settings/bond.h
+++ settings/bond.h
@@ -22,7 +22,7 @@
 #define NMQT_SETTINGS_BOND_H
 
 #include "../NetworkManagerQt-export.h"
-#include "generic-types.h"
+#include "../generic-types.h"
 #include "setting.h"
 
 #include <QtCore/QString>
