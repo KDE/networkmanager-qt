48864443e5b8ff45ae6077b0113de2f6eac59a56
Lamarque V. Souza <lamarque@kde.org>
2013-06-09 12:22:46 -0300

    Remove unused class.

--- device_p.h
+++ device_p.h
@@ -22,7 +22,6 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 #define NMQT_DEVICE_P_H
 
 #include "dbus/nm-deviceinterface.h"
-#include "nmpropertyhelper.h"
 
 namespace NetworkManager
 {
