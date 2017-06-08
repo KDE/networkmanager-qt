c676f0f66ae7825958494a6ca2970af98878c2ee
Jan Grulich <jgrulich@redhat.com>
2017-06-08 14:20:17 +0200

    Extend test for new properties from connection settings

--- autotests/settings/connectionsettingtest.cpp
+++ autotests/settings/connectionsettingtest.cpp
@@ -43,21 +43,37 @@ void ConnectionSettingTest::testSetting_data()
     QTest::addColumn<QString>("zone");
     QTest::addColumn<QString>("master");
     QTest::addColumn<QString>("slaveType");
+    QTest::addColumn<QStringList>("secondaries");
+    QTest::addColumn<uint>("gatewayPingTimeout");
+    QTest::addColumn<int>("autoconnectPriority");
+    QTest::addColumn<int>("autoconnectRetries");
+    QTest::addColumn<int>("autoconnectSlaves");
+    QTest::addColumn<int>("lldp");
+    QTest::addColumn<int>("metered");
+    QTest::addColumn<QString>("stableId");
 
     QStringList permissions;
     permissions << "user:foo:bar";
 
     QTest::newRow("setting1")
-            << QString("test")                                  // id
-            << QString("2815492f-7e56-435e-b2e9-246bd7cdc664")  // uuid
-            << QString("802-3-ethernet")                        // type
-            << permissions                                      // permissions
-            << false                                            // autoconnect
-            << (quint64) 1355945444                             // timestamp
-            << true                                             // readOnly
-            << QString("Home")                                  // zone
-            << QString("3ba0ba8c-58f3-427d-bce4-3c8200277a5c")  // master
-            << QString("bond");                                 // slaveType
+            << QString("test")                                      // id
+            << QString("2815492f-7e56-435e-b2e9-246bd7cdc664")      // uuid
+            << QString("802-3-ethernet")                            // type
+            << permissions                                          // permissions
+            << false                                                // autoconnect
+            << (quint64) 1355945444                                 // timestamp
+            << true                                                 // readOnly
+            << QString("Home")                                      // zone
+            << QString("3ba0ba8c-58f3-427d-bce4-3c8200277a5c")      // master
+            << QString("bond")                                      // slaveType
+            << QStringList{{"2d331115-a2f4-4376-b24e-b8ba6e71abcf"}}// secondaries
+            << (uint)10                                             // gatewayPingTimeout
+            << 10                                                   // autoconnectPriority
+            << 5                                                    // autoconnectRetries
+            << 1                                                    // autoconnectSlaves
+            << 1                                                    // lldp
+            << 1                                                    // metered
+            << QString("${CONNECTION}/${BOOT}");                    // stableId
 }
 
 void ConnectionSettingTest::testSetting()
@@ -72,6 +88,14 @@ void ConnectionSettingTest::testSetting()
     QFETCH(QString, zone);
     QFETCH(QString, master);
     QFETCH(QString, slaveType);
+    QFETCH(QStringList, secondaries);
+    QFETCH(uint, gatewayPingTimeout);
+    QFETCH(int, autoconnectPriority);
+    QFETCH(int, autoconnectRetries);
+    QFETCH(int, autoconnectSlaves);
+    QFETCH(int, lldp);
+    QFETCH(int, metered);
+    QFETCH(QString, stableId);
 
     NMVariantMapMap mapmap;
     QVariantMap map;
@@ -86,6 +110,14 @@ void ConnectionSettingTest::testSetting()
     map.insert(QLatin1String(NM_SETTING_CONNECTION_ZONE), zone);
     map.insert(QLatin1String(NM_SETTING_CONNECTION_MASTER), master);
     map.insert(QLatin1String(NM_SETTING_CONNECTION_SLAVE_TYPE), slaveType);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_SECONDARIES), secondaries);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_GATEWAY_PING_TIMEOUT), gatewayPingTimeout);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_AUTOCONNECT_PRIORITY), autoconnectPriority);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_AUTOCONNECT_RETRIES), autoconnectRetries);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_AUTOCONNECT_SLAVES), autoconnectSlaves);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_LLDP), lldp);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_METERED), metered);
+    map.insert(QLatin1String(NM_SETTING_CONNECTION_STABLE_ID), stableId);
 
     mapmap.insert(QLatin1String(NM_SETTING_CONNECTION_SETTING_NAME), map);
 
